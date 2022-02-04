36e88785eccaa7d6c9b2c22db6e2bd650a988610
Denis Mythnik <themrcrovvley@gmail.com>
2022-02-04 13:56:20 +0300

    feat: add AppRouter

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { signinAfterRegistration } from '../redux/store/reducers/authReducer';
+import Layout from './main-page/Layout';
+import HomePage from './pages/HomePage';
+import RegistrationPage from './pages/RegistrationPage';
+
+const AppRouter = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.getItem('userData')) {
+      dispatch(signinAfterRegistration());
+    }
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="home" element={<HomePage />} />
+        <Route path="registration" element={<RegistrationPage />} />
+        {/* <Route path="book" element={<Ebook />} />
+        <Route path="games" element={<GamesPage />} />
+        <Route path="statistics" element={<StatisticsPage />} />
+        <Route path="team" element={<TeamPage />} /> */}
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRouter;
