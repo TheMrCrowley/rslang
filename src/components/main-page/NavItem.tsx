b02ce24150bccf84d05254f3d36c46562efee3c0
Denis Mythnik <themrcrovvley@gmail.com>
2022-02-04 13:57:05 +0300

    feat: add routin provider, redux provider and theme provider

--- src/components/main-page/NavItem.tsx
+++ src/components/main-page/NavItem.tsx
@@ -3,12 +3,11 @@ import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 type NavItemProps = {
-  children: React.ReactElement | React.ReactNode;
   innerText: string;
   onClick: () => void;
 };
 
-const NavItem: FC<NavItemProps> = ({ children, innerText, onClick }) => {
+const NavItem: FC<NavItemProps> = ({ innerText, onClick, children }) => {
   const navigate = useNavigate();
   return (
     <ListItem
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
-import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
 import App from './App';
 import store from './redux/store';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#202026',
+    },
+    secondary: {
+      main: '#fecb00',
+    },
+  },
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <CssBaseline />
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CssBaseline />
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
