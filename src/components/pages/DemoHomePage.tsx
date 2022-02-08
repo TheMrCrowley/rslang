e9559c8a07b172cb2c78c3377599c17d0c8a9454
Alexander Suslov <sasha_suslov07@mail.ru>
2022-02-08 12:16:10 +0300

    feat: App-design(RL-4) add buttons to main page (registration, about)

--- src/components/pages/DemoHomePage.tsx
+++ src/components/pages/DemoHomePage.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography, ButtonGroup, styled } from '@mui/material';
 import React from 'react';
+import Grid from '@mui/material/Grid';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
@@ -8,21 +9,6 @@ import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
 import { RootState } from '../../redux/store';
 import Bg from '../../assets/Untitled.png';
 import MainPageLayoutButton from './MainPageLayoutButton';
-
-// const StyledButton = styled(Button)`
-//   background-color: #fecb00;
-//   border: 2px solid transparent;
-//   font-size: 24px;
-//   &:hover {
-//     background-color: #202026;
-//     color: #fecb00;
-//     border: 2px solid #fecb00;
-//   }
-// `;
-
-// const SyledMainPageWrpper = styled(Box)`
-
-// `;
 
 const StyledButtonGroup = styled(ButtonGroup)`
   display: flex;
@@ -36,115 +22,99 @@ const DemoHomePage = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        height: '100vh',
+        flexGrow: 1,
+        p: '5em 2em 0 6em',
         backgroundImage: `url(${Bg})`,
         backgroundSize: 'cover',
       }}
     >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          p: '5em 0 0 10em',
-        }}
-      >
-        <Typography
-          variant="h1"
-          component="h1"
-          sx={{
-            backgroundColor: '#99ffe0',
-            borderRadius: '3em',
-            p: '0.5em 1em 0.5em 1em',
-
-            fontWeight: 'bold',
-          }}
-        >
-          RS Lang
-        </Typography>
-        <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
-          Play and study English for free
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            columnGap: '2em',
-          }}
-        >
+      <Grid container rowSpacing={1} columnSpacing={1}>
+        <Grid item xs>
+          <Typography
+            variant="h1"
+            component="h1"
+            sx={{
+              maxWidth: '6em',
+              backgroundColor: '#99ffe0',
+              borderRadius: '3em',
+              p: '0.4em 1em 0.4em 1em',
+              textAlign: 'center',
+              fontWeight: 'bold',
+            }}
+          >
+            RS Lang
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <Typography
+            variant="h3"
+            sx={{
+              maxWidth: '13em',
+              color: '#14cba8',
+              fontWeight: 'bold',
+              ml: 'auto',
+              p: '0.8em',
+              borderRadius: '3em',
+              backgroundColor: '#ffffff',
+            }}
+          >
+            Create account and get more options!
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            variant="h3"
+            sx={{
+              mb: '10%',
+              fontWeight: 'bold',
+            }}
+          >
+            Play and study English for free
+          </Typography>
+        </Grid>
+        <Grid item xs>
           <MainPageLayoutButton onClick={() => navigate('/book')} text="E-book">
             <MenuBookIcon fontSize="large" />
           </MainPageLayoutButton>
+        </Grid>
+        <Grid item xs>
           <MainPageLayoutButton onClick={() => null} text="Sprint">
             <DirectionsRunIcon fontSize="large" />
           </MainPageLayoutButton>
+        </Grid>
+        <Grid item xs>
           <MainPageLayoutButton onClick={() => null} text="Savanah">
             <EmojiNatureIcon fontSize="large" />
           </MainPageLayoutButton>
-        </Box>
-        <Box
-          sx={{
-            alignSelf: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-            flexWrap: 'wrap',
-            rowGap: '0.5em',
-            justifyContent: 'space-evenly',
-            alignItems: 'center',
-            color: '#000',
-            borderRadius: '3em',
-            backgroundColor: '#fff',
-            p: '2em',
-          }}
-        >
-          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
-            More than 3500 words!
-          </Typography>
-          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
-            Two amazing games!
-          </Typography>
-          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
-            Detailed progress statistics for authorized users!
-          </Typography>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          ml: 'auto',
-          mt: '4em',
-        }}
-      >
-        <Typography
-          variant="h3"
-          sx={{
-            color: '#14cba8',
-            fontWeight: 'bold',
-            p: '1em',
-            borderRadius: '3em',
-            backgroundColor: '#ffffff',
-          }}
-        >
-          Create account and get
-          <br /> more options!
-        </Typography>
-        <StyledButtonGroup>
+        </Grid>
+        <Grid item xs>
           {!isAuth && (
             <MainPageLayoutButton
               onClick={() => navigate('/registration')}
               text="Registration"
             />
           )}
+        </Grid>
+        <Grid item xs>
           <MainPageLayoutButton text="About App" />
-        </StyledButtonGroup>
-      </Box>
+        </Grid>
+        <Grid item xs={12} height="4em" />
+        <Grid item xs>
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+            More than 3500 words!
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+            Two amazing games!
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+            Detailed progress statistics for authorized users!
+          </Typography>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
