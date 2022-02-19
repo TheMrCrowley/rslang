af98dd013718f37430aedec0d81b06fcc664fd51
TheMrCrowley <themrcrovvley@gmail.com>
2022-02-20 00:57:59 +0300

    feat: add sticky to header and bookbar

--- src/components/e-book/ui/bookbar/BookBar.tsx
+++ src/components/e-book/ui/bookbar/BookBar.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import { Button, Tooltip, Typography } from '@mui/material';
+import { Box, Button, Tooltip, Typography } from '@mui/material';
 import BasicSelect from '../select/BasicSelect';
 import PaginationRanges from '../pagination/PaginationRanges';
-import { DIFFICULT_GROUP } from '../../cosnstants';
+import { colors, DIFFICULT_GROUP } from '../../cosnstants';
 import useBookParams from '../../../../hooks/useBookParams';
 import useSprintFromBook from '../../../sprint/useSprintFromBook';
 import useAudiocallFromBook from '../../../audiocall/useAudiocallFromBook';
+import useWindowScroll from '../../../../hooks/useWindowScroll';
 
 interface ResponsiveAppBarProps {
   isAuth: boolean;
@@ -22,13 +23,15 @@ const theme = createTheme({
   },
 });
 
-const StyledBarContainer = styled(Container)({
+const StyledBarContainer = styled(Box)({
+  width: '100%',
   display: 'flex',
   justifyContent: 'space-evenly',
   alignItems: 'center',
   height: '4.5em',
   position: 'sticky',
-  top: '0px',
+
+  zIndex: 100,
 });
 
 const GameButton = styled(Button)`
@@ -36,7 +39,7 @@ const GameButton = styled(Button)`
   color: #202026;
   background-color: white;
   border-radius: 2rem;
-  &: hover {
+  &:hover {
     color: white;
     background-color: #202026;
   }
@@ -53,8 +56,11 @@ const BookBar: React.FC<ResponsiveAppBarProps> = ({
   const { group, page } = useBookParams();
   const sprintHandler = useSprintFromBook(isAuth, group, page, userId);
   const audioCallHandler = useAudiocallFromBook(isAuth, group, page, userId);
+  const top = useWindowScroll(64);
   return (
-    <StyledBarContainer maxWidth="xl">
+    <StyledBarContainer
+      sx={{ top: `${top}px`, backgroundColor: colors[group] }}
+    >
       <Tooltip title="Play game with unstudied words from this page">
         <GameButton onClick={sprintHandler}>Sprint</GameButton>
       </Tooltip>
--- src/components/main-page/Header.tsx
+++ src/components/main-page/Header.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
-import { AppBar, Toolbar } from '@mui/material';
 import Nav from './Nav';
 import AuthButton from './AuthButton';
+import useWindowScroll from '../../hooks/useWindowScroll';
 
 const StyledContainer = styled(Container)`
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
+
+const StyledHeader = styled('header')({
+  position: 'sticky',
+  backgroundColor: '#fff',
+  minHeight: '64px',
+  display: 'flex',
+  alignItems: 'center',
+  zIndex: 100,
+});
+
 const Header = () => {
+  const top = useWindowScroll(0);
   return (
-    <Toolbar component="header">
+    <StyledHeader sx={{ top: `${top}px` }}>
       <StyledContainer>
         <Nav />
         <AuthButton />
       </StyledContainer>
-    </Toolbar>
+    </StyledHeader>
   );
 };
 
